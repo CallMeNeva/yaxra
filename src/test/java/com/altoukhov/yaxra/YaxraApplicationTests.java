ee5a22c6015e037343960d3e7d07803d64154909
Max Altoukhov <max@altoukhov.com>
2022-11-12 03:09:49 +0200

    Fresh Spring Initializr init

---
+++ src/test/java/com/altoukhov/yaxra/YaxraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altoukhov.yaxra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaxraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
