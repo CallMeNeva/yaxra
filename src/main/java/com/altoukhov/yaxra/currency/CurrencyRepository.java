9fc029d7e76493f556afadced46dc1ed8a8512c3
Max Altoukhov <max@altoukhov.com>
2022-11-12 23:57:27 +0200

    Add CurrencyRepository

---
+++ src/main/java/com/altoukhov/yaxra/currency/CurrencyRepository.java
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: Unlicense
+
+package com.altoukhov.yaxra.currency;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CurrencyRepository extends JpaRepository<Currency, Short> {
+}
