ec757c3fa63ded27973c47add47691a70ab5129d
Max Altoukhov <max@altoukhov.com>
2022-11-14 22:07:46 +0200

    Add currency by ID endpoint

--- src/main/java/com/altoukhov/yaxra/currency/CurrencyController.java
+++ src/main/java/com/altoukhov/yaxra/currency/CurrencyController.java
@@ -3,14 +3,18 @@
 package com.altoukhov.yaxra.currency;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
+import java.util.Optional;
 
 import static java.util.stream.Collectors.toMap;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -31,5 +35,18 @@ public class CurrencyController {
         return repository.findAll()
                 .stream()
                 .collect(toMap(Currency::getFormattedNumericCode, Currency::getName));
+    }
+
+    @GetMapping(value = "/{id}", produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<CurrencyGetDTO> byId(@PathVariable String id) {
+        try {
+            short numericCode = Short.parseShort(id);
+            Optional<Currency> entity = repository.findById(numericCode);
+
+            Optional<CurrencyGetDTO> dto = entity.map(e -> new CurrencyGetDTO(e.getAlphabeticCode(), e.getName()));
+            return ResponseEntity.of(dto);
+        } catch (NumberFormatException nfe) {
+            return new ResponseEntity<>(BAD_REQUEST);
+        }
     }
 }
---
+++ src/main/java/com/altoukhov/yaxra/currency/CurrencyGetDTO.java
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: Unlicense
+
+package com.altoukhov.yaxra.currency;
+
+public record CurrencyGetDTO(String alphaCode, String name) {}
