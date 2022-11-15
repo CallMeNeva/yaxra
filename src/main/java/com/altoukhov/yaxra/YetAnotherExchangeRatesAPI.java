6ae31c59ece5309e30f437d522f8d32a6b796c2c
Max Altoukhov <max@altoukhov.com>
2022-11-15 18:15:22 +0200

    Prettify entry point class name

--- src/main/java/com/altoukhov/yaxra/YetAnotherExchangeRatesAPI.java
+++ src/main/java/com/altoukhov/yaxra/YetAnotherExchangeRatesAPI.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class YaxraApplication {
+public class YetAnotherExchangeRatesAPI {
 
     public static void main(String[] args) {
-        SpringApplication.run(YaxraApplication.class, args);
+        SpringApplication.run(YetAnotherExchangeRatesAPI.class, args);
     }
-
 }
