51b2a3164052e4fa2a6f70c482da48b083ca25e1
Max Altoukhov <max@altoukhov.com>
2022-11-15 16:34:23 +0200

    Add Exchange

---
+++ src/main/java/com/altoukhov/yaxra/exchange/Exchange.java
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: Unlicense
+
+package com.altoukhov.yaxra.exchange;
+
+import com.altoukhov.yaxra.currency.Currency;
+import org.springframework.lang.NonNull;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+@Embeddable
+@Access(AccessType.FIELD)
+public class Exchange {
+
+    @ManyToOne
+    @JoinColumn(name = "base_num_code", nullable = false)
+    private @NonNull Currency baseCurrency;
+
+    @ManyToOne
+    @JoinColumn(name = "target_num_code", nullable = false)
+    private @NonNull Currency targetCurrency;
+
+    @Column(name = "date", nullable = false)
+    private @NonNull LocalDate date;
+
+    public @NonNull Currency getBaseCurrency() {
+        return baseCurrency;
+    }
+
+    public @NonNull Currency getTargetCurrency() {
+        return targetCurrency;
+    }
+
+    public @NonNull LocalDate getDate() {
+        return date;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof Exchange that
+                && Objects.equals(this.baseCurrency, that.baseCurrency)
+                && Objects.equals(this.targetCurrency, that.targetCurrency)
+                && Objects.equals(this.date, that.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(baseCurrency, targetCurrency, date);
+    }
+
+    @Override
+    public String toString() {
+        return "%s --> %s at %s".formatted(
+                baseCurrency.getAlphabeticCode(),
+                targetCurrency.getAlphabeticCode(),
+                date.format(DateTimeFormatter.ISO_LOCAL_DATE)
+        );
+    }
+}
