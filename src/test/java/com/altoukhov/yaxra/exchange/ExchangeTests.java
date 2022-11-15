af2589a5009990c933b596288ce3cf75158f8ff0
Max Altoukhov <max@altoukhov.com>
2022-11-15 17:10:36 +0200

    Add Exchange::createValidated tests

--- src/test/java/com/altoukhov/yaxra/exchange/ExchangeTests.java
+++ src/test/java/com/altoukhov/yaxra/exchange/ExchangeTests.java
@@ -3,9 +3,45 @@
 package com.altoukhov.yaxra.exchange;
 
 import com.altoukhov.yaxra.TestUtils;
+import com.altoukhov.yaxra.currency.Currency;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assumptions.assumeThat;
+
 class ExchangeTests {
+
+    @Nested
+    class CreateValidated {
+
+        @Test
+        void throwsIAEOnEqualCurrencies() {
+            Currency base = Currency.createUnvalidated((short) 1, "FOO", "Foo Dollar");
+            Currency target = Currency.createUnvalidated((short) 1, "FOO", "Foo Dollar");
+
+            assumeThat(base.equals(target))
+                    .isTrue();
+
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> Exchange.createValidated(base, target, LocalDate.EPOCH));
+        }
+
+        @Test
+        void doesNotThrowOnNonEqualCurrencies() {
+            Currency base = Currency.createUnvalidated((short) 1, "FOO", "Foo Dollar");
+            Currency target = Currency.createUnvalidated((short) 2, "BAR", "Bar Dollar");
+
+            assumeThat(base.equals(target))
+                    .isFalse();
+
+            assertThatNoException()
+                    .isThrownBy(() -> Exchange.createValidated(base, target, LocalDate.EPOCH));
+        }
+    }
 
     @Test
     void equalsContract() {
